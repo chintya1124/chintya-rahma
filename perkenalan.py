3ea14c18d4b78889ffa563bdd4f89e0221b8a230
chintya1124 <94589639+chintya1124@users.noreply.github.com>
2021-11-18 11:43:23 +0700

    Add files via upload

---
+++ perkenalan.py
@@ -0,0 +1,11 @@
+nama = "Chintya Dwi Rahmawati" 
+NIM = '212410102099' 
+umur = 19  
+tinggiBadan = 1.57  
+beratBadan = 40.5   
+
+print(nama)
+print(NIM)
+print(umur)
+print(tinggiBadan)
+print(beratBadan)
